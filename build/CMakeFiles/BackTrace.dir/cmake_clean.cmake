18d3de72e64f2905450179e7cec8c42f455c7960
waleed <waleed@Ubuntu.myguest.virtualbox.org>
2025-01-17 18:23:27 +0200

    using CMake

---
+++ build/CMakeFiles/BackTrace.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "BackTrace"
+  "BackTrace.pdb"
+  "CMakeFiles/BackTrace.dir/Backtrace.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BackTrace.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
